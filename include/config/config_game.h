54895e3e4655a43a27031dfbfcd45b19c360ad84
ariahiro64 <ariahiro64@gmail.com>
2022-08-07 01:12:57 -0400

    add to antialiasing config options

--- include/config/config_game.h
+++ include/config/config_game.h
@@ -1,4 +1,6 @@
 #ifndef CONFIG_GAME_H
 #define CONFIG_GAME_H
 
+#include "config_graphics.h"
+
 #endif
---
+++ include/config/config_graphics.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/*********************
+ * GRAPHICS SETTINGS *
+ *********************/
+
+// Disables AA (Improves console performance but causes visible seams between unconnected geometry).
+// #define DISABLE_AA
+
