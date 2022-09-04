2003660ed7d564763a65c4a72dfde53f1202e89f
Recardo <108219836+Recardo-7@users.noreply.github.com>
2022-09-04 22:02:52 +0200

    Some QOL improvements (#2)
    * Add text speed option
    
    * Rupee symbol changes color with wallet upgrades
    
    * fast bunnyhood
    
    * Add block push speed option
    
    * Fix pause screen crash
    
    * No low health beep option
    
    * Fix crash debugger & remove button combo
    
    * Remove button combo function entirely
    
    * Better wallet color change
    
    * Make block push speed multiplier a float
    
    * Rename text speed
    
    * Change low health beep option
    
    * Remove MM bunnyhood (better implementation in #10)
    
    * include config.h
    
    Co-authored-by: Zelllll <56516451+Zelllll@users.noreply.github.com>
    
    * fix errors
    
    * Rename WALLET_COLORS
    
    * Fixes
    
    Co-authored-by: Zelllll <56516451+Zelllll@users.noreply.github.com>

--- src/code/z_parameter.c
+++ src/code/z_parameter.c
@@ -4,6 +4,8 @@
 #include "assets/textures/do_action_static/do_action_static.h"
 #include "assets/textures/icon_item_static/icon_item_static.h"
 
+#include "config.h"
+
 // TODO extract this information from the texture definitions themselves
 #define DO_ACTION_TEX_WIDTH 48
 #define DO_ACTION_TEX_HEIGHT 16
@@ -3101,7 +3103,18 @@ void Interface_Draw(PlayState* play) {
         Gfx_SetupDL_39Overlay(play->state.gfxCtx);
 
         // Rupee Icon
+        #ifndef MM_WALLET_ICON_COLORS
         gDPSetPrimColor(OVERLAY_DISP++, 0, 0, 200, 255, 100, interfaceCtx->magicAlpha);
+        #else
+        static Color_RGB8 const walletColors[] = {
+            { 200, 255, 100 },
+            { 130, 130, 255 },
+            { 255, 100, 100 },
+        };
+        u8 walletUpg = CUR_UPG_VALUE(UPG_WALLET);
+        gDPSetPrimColor(OVERLAY_DISP++, 0, 0, walletColors[walletUpg].r, walletColors[walletUpg].g, walletColors[walletUpg].b, interfaceCtx->magicAlpha);
+        #endif
+        
         gDPSetEnvColor(OVERLAY_DISP++, 0, 80, 0, 255);
         OVERLAY_DISP = Gfx_TextureIA8(OVERLAY_DISP, gRupeeCounterIconTex, 16, 16, 26, 206, 16, 16, 1 << 10, 1 << 10);
 
