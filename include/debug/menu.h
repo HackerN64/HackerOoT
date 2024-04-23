81d394640b474423c94b949a14cd5d448a695604
Yanis42 <35189056+Yanis42@users.noreply.github.com>
2024-04-23 12:19:32 +0200

    started collision view

--- include/debug/menu.h
+++ include/debug/menu.h
@@ -1,6 +1,36 @@
 #ifndef HACKEROOT_MENU_H
 #define HACKEROOT_MENU_H
 
+#define qs105(n) ((s16)((n) * 0x0020))
+
+#define gdSPDefVtxN(x, y, z, s, t, nx, ny, nz, ca) \
+	( \
+		(Vtx) \
+		{ \
+			.n = \
+			{ \
+				.ob = {x, y, z}, \
+				.tc = {qs105(s), qs105(t)}, \
+				.n = {nx, ny, nz}, \
+				.a = ca \
+			} \
+		} \
+	)
+
+#define gDisplayListData(pgdl, d) \
+({ \
+	Gfx **Gp_ = (void *)(pgdl); \
+	struct \
+	{ \
+		__typeof__(d) v; \
+	} *Gd_, *Gs_; \
+	*Gp_ -= (sizeof(*Gd_) + sizeof(Gfx) - 1) / sizeof(Gfx); \
+	Gd_ = (void *)*Gp_; \
+	Gs_ = (void *)&(d); \
+	*Gd_ = *Gs_; \
+	&Gd_->v; \
+})
+
 typedef u8 (*MenuFunc)(void*);
 
 typedef enum MenuSelection {
@@ -12,6 +42,51 @@ typedef enum MenuSelection {
     MENU_DUMMY4,
     MENU_MAX
 } MenuSelection;
+
+typedef enum MenuSurfaceWallParams {
+    SURFACE_WALL_NO_LEDGE_GRAB = 0x1,
+    SURFACE_WALL_LADDER = 0x2,
+    SURFACE_WALL_LADDER_TOP = 0x3,
+    SURFACE_WALL_VINE = 0x4,
+    SURFACE_WALL_CRAWL_A = 0x5,
+    SURFACE_WALL_CRAWL_B = 0x6,
+    SURFACE_WALL_PUSH = 0x7,
+} MenuSurfaceWallParams;
+
+typedef enum MenuSurfaceFloorParams {
+    SURFACE_FLOOR_VOID_SMALL = 0x5,
+    SURFACE_FLOOR_HANG_LEDGE = 0x6,
+    SURFACE_FLOOR_STOP_AIR_MOMENTUM = 0x8,
+    SURFACE_FLOOR_NO_LEDGE_JUMP = 0x9,
+    SURFACE_FLOOR_DIVE = 0xB,
+    SURFACE_FLOOR_VOID = 0xC,
+} MenuSurfaceFloorParams;
+
+typedef struct MenuPolygonTypes {
+    // HIGH
+    struct {
+        u32 blockEpona   : 1;
+        u32 lowerSurface : 1;
+        u32 floorParams  : 4;
+        u32 wallParams   : 5;
+        u32 unk_001C0000 : 3;
+        u32 behaviour    : 5;
+        u32 exit         : 5;
+        u32 camDataIndex : 8;
+    };
+    // LOW
+    struct {
+        u32 pad           : 4;
+        u32 wallDamage    : 1;
+        u32 conveyorDir   : 6;
+        u32 conveyorSpeed : 3;
+        u32 hookshot      : 1;
+        u32 echo          : 6;
+        u32 lightParams   : 5;
+        u32 slope         : 2;
+        u32 sfx           : 4;
+    };
+} MenuPolygonTypes;
 
 typedef struct MenuElement {
     char* name;
@@ -26,6 +101,7 @@ typedef struct Menu {
     u8 bExecute;
     u8 bReturn;
     u8 nTimer;
+    u8 bColViewEnabled;
     MenuSelection eSelection;
     struct PlayState* pPlay;
     Input* pInput;
@@ -34,5 +110,6 @@ typedef struct Menu {
 void Menu_Init(Menu* this);
 void Menu_Update(Menu* this);
 void Menu_Draw(Menu* this);
+void Menu_DrawCollisionView(Menu* this);
 
 #endif
--- src/debug/menu.c
+++ src/debug/menu.c
@@ -1,27 +1,63 @@
 #include "global.h"
 #include "debug.h"
 
+#define MENU_BUFFER_SIZE 0x4000
+#define ALIGN(val, align) ((((val) % (align)) != 0) ? (val) + (align) - ((val) % (align)) : (val))
 
-u8 test(Menu* this) {
-    CollisionCheck_DrawCollision(this->pPlay, &this->pPlay->colChkCtx);
-    return true;
-}
+static Gfx sPolyBuffer[MENU_BUFFER_SIZE];
+
+static Gfx sPolyGfxInit_HitBox[] = {
+	gsSPLoadGeometryMode(G_ZBUFFER | G_SHADE | G_LIGHTING),
+	gsSPTexture(0, 0, 0, G_TX_RENDERTILE, G_OFF),
+	gsDPPipeSync(),
+	gsDPSetCycleType(G_CYC_1CYCLE),
+	gsDPSetRenderMode(
+		Z_CMP | IM_RD | CVG_DST_FULL | FORCE_BL | ZMODE_XLU | GBL_c1(G_BL_CLR_IN,G_BL_A_IN, G_BL_CLR_MEM, G_BL_1MA),
+		Z_CMP | IM_RD | CVG_DST_FULL | FORCE_BL | ZMODE_XLU | GBL_c2(G_BL_CLR_IN,G_BL_A_IN, G_BL_CLR_MEM, G_BL_1MA)
+	),
+	gsDPSetCombineLERP(PRIMITIVE, 0, SHADE, 0, 0, 0, 0, ENVIRONMENT, PRIMITIVE, 0, SHADE, 0, 0, 0, 0, ENVIRONMENT),
+	gsDPSetEnvColor(255, 255, 255, 128),
+	gsSPEndDisplayList(),
+};
+
+static Gfx sPolyGfxInit_Collision[] = {
+	gsSPLoadGeometryMode(G_ZBUFFER | G_SHADE | G_LIGHTING),
+	gsSPTexture(0, 0, 0, G_TX_RENDERTILE, G_OFF),
+	gsDPPipeSync(),
+	gsDPSetCycleType(G_CYC_1CYCLE),
+	gsDPSetRenderMode(
+		Z_CMP | IM_RD | CVG_DST_FULL | FORCE_BL | ZMODE_DEC | GBL_c1(G_BL_CLR_IN,G_BL_A_IN, G_BL_CLR_MEM, G_BL_1MA),
+		Z_CMP | IM_RD | CVG_DST_FULL | FORCE_BL | ZMODE_DEC | GBL_c2(G_BL_CLR_IN,G_BL_A_IN, G_BL_CLR_MEM, G_BL_1MA)
+	),
+	gsDPSetCombineLERP(PRIMITIVE, 0, SHADE, 0, 0, 0, 0, ENVIRONMENT, PRIMITIVE, 0, SHADE, 0, 0, 0, 0, ENVIRONMENT),
+	gsDPSetEnvColor(255, 255, 255, 128),
+	gsSPEndDisplayList(),
+};
 
 static MenuElement sMenuElements[] = {
 #if ENABLE_F3DEX3
     { "Profiler", false, (void*)&gProfiler, (MenuFunc)Profiler_Update, (MenuFunc)Profiler_Draw },
 #endif
-    { "Collision View", true, (void*)&gDebug.menu, NULL, (MenuFunc)test },
+    { "Collision View", true, (void*)&gDebug.menu, NULL, NULL },
     { "Dummy 2", false, NULL, NULL, NULL },
     { "Dummy 3", false, NULL, NULL, NULL },
     { "Dummy 4", false, NULL, NULL, NULL },
 };
+
+f32 MaxF(f32 a, f32 b) {
+    return a >= b ? a : b;
+}
+
+f32 MinF(f32 a, f32 b) {
+    return a < b ? a : b;
+}
 
 void Menu_Init(Menu* this) {
     this->bShow = false;
     this->bExecute = false;
     this->bReturn = false;
     this->nTimer = 0;
+    this->bColViewEnabled = false;
     this->eSelection = MENU_MIN + 1;
     this->pPlay = gDebug.play;
     this->pInput = gDebug.input;
@@ -34,7 +70,6 @@ void Menu_Update(Menu* this) {
         u8 isHoldingR = CHECK_BTN_ALL(curBtn, BTN_R);
         u8 i;
 
-        test(this);
         // if (gSaveContext.gameMode != GAMEMODE_NORMAL || IS_PAUSED(&this->pPlay->pauseCtx)) {
         //     return;
         // }
@@ -95,6 +130,8 @@ void Menu_Draw(Menu* this) {
     PrintUtils* print = &gDebug.printer;
     u8 i;
 
+    Menu_DrawCollisionView(this);
+
     Debug_DrawColorRectangle(left, right, bg);
 
     Print_Screen(print, 11, 3, COLOR_WHITE, "--- Debug Menu ---");
@@ -126,4 +163,141 @@ void Menu_Draw(Menu* this) {
             PRINTF("[HackerOoT:Menu]: an error occurred while trying to run the draw function\n");   
         }
     }
+}
+
+void Menu_IsOutOfMemory(Gfx** cur, Gfx** head, const char* msg) {
+    if (*cur > *head) {
+        Fault_AddHungupAndCrashImpl(msg, "[HackerOoT:FATAL]: Gfx Buffer ran out of space!\n");
+    }
+}
+
+void Menu_DrawStaticCollision(Menu* this, Gfx** gfxP, Gfx** gfxD, CollisionHeader* colHeader) {
+    PlayState* play = this->pPlay;
+    s32 prevTyid = -1;
+    s16 alpha = -1;
+    Camera* cam = GET_ACTIVE_CAM(play);
+    s32 i, j;
+    
+    if (colHeader == NULL) {
+        return;
+    }
+    
+    for (i = 0; i < colHeader->numPolygons; i++) {
+        CollisionPoly* colPoly = &colHeader->polyList[i];
+        MenuPolygonTypes* type = (MenuPolygonTypes*)&colHeader->surfaceTypeList[colPoly->type];
+        s32 tyid = -1;
+        Vec3s vPos[3];
+        f32 dist = MAXFLOAT;
+        
+        for (j = 0; j < 3; j++) {
+            vPos[j].x = colHeader->vtxList[colPoly->vtxData[j] & 0x1FFF].x;
+            vPos[j].y = colHeader->vtxList[colPoly->vtxData[j] & 0x1FFF].y;
+            vPos[j].z = colHeader->vtxList[colPoly->vtxData[j] & 0x1FFF].z;
+            
+            dist = MinF(
+                sqrtf(SQ(vPos[j].x - cam->eye.x) + SQ(vPos[j].y - cam->eye.y) + SQ(vPos[j].z - cam->eye.z)),
+                dist
+            );
+        }
+        
+        if (dist > 950.0f) {
+            continue;
+        }
+        
+        if (type->hookshot == 1) {
+            tyid = 0;
+        } else if (type->wallParams > SURFACE_WALL_NO_LEDGE_GRAB) {
+            tyid = 1;
+        } else if (type->floorParams == SURFACE_FLOOR_VOID) {
+            tyid = 2;
+        } else if (type->exit != 0 || type->floorParams == SURFACE_FLOOR_VOID_SMALL) {
+            tyid = 3;
+        } else if (type->behaviour != 0 || type->wallDamage) {
+            tyid = 4;
+        } else if (type->slope == 1) {
+            tyid = 5;
+        } else {
+            tyid = 6;
+        }
+
+        if (tyid != prevTyid) {
+            switch (tyid) {
+                case 0:
+                    gDPSetPrimColor((*gfxP)++, 0, 0, 128, 128, 255, 255);
+                    break;
+                case 1:
+                    gDPSetPrimColor((*gfxP)++, 0, 0, 192, 0, 192, 255);
+                    break;
+                case 2:
+                    gDPSetPrimColor((*gfxP)++, 0, 0, 255, 0, 0, 255);
+                    break;
+                case 3:
+                    gDPSetPrimColor((*gfxP)++, 0, 0, 0, 255, 0, 255);
+                    break;
+                case 4:
+                    gDPSetPrimColor((*gfxP)++, 0, 0, 192, 255, 192, 255);
+                    break;
+                case 5:
+                    gDPSetPrimColor((*gfxP)++, 0, 0, 255, 255, 128, 255);
+                    break;
+                case 6:
+                    gDPSetPrimColor((*gfxP)++, 0, 0, 255, 255, 255, 255);
+                    break;
+            }
+            prevTyid = tyid;
+        }
+        
+        Vtx vtx[3];
+        s16 nextAlpha;
+        if (dist > 100.0f) {
+            f32 mul = 1.0f - (dist - 100) * 0.00111111111f;
+            nextAlpha = 185 * CLAMP_MAX(mul, 1.0f);
+            nextAlpha = ALIGN(nextAlpha, 4);
+            nextAlpha = CLAMP(nextAlpha, 0, 185);
+        } else {
+            nextAlpha = 185;
+        }
+        
+        if (alpha != nextAlpha) {
+            gDPSetEnvColor((*gfxP)++, 192, 0, 192, nextAlpha);
+        }
+        
+        for (j = 0; j < 3; j++) {
+            vtx[j] = gdSPDefVtxN(
+                vPos[j].x,
+                vPos[j].y,
+                vPos[j].z,
+                0,
+                0,
+                colPoly->normal.x / 0x100,
+                colPoly->normal.y / 0x100,
+                colPoly->normal.z / 0x100,
+                0xFF
+            );
+        }
+
+        gSPVertex((*gfxP)++, gDisplayListData(gfxD, vtx), 3, 0);
+        gSP1Triangle((*gfxP)++, 0, 1, 2, 0);
+        Menu_IsOutOfMemory(gfxP, gfxD, "Scene Collision");
+    }
+}
+
+void Menu_DrawCollisionView(Menu* this) {
+    Gfx* dlist = sPolyBuffer;
+    Gfx* pGfx = dlist;
+    Gfx* dGfx = dlist + MENU_BUFFER_SIZE;
+
+    if (!this->bColViewEnabled) {
+        return;
+    }
+
+    OPEN_DISPS(this->pPlay->state.gfxCtx, __FILE__, __LINE__);
+
+    gSPDisplayList(pGfx++, sPolyGfxInit_Collision);
+    gSPSetGeometryMode(pGfx++, G_CULL_BACK);
+    gSPMatrix(pGfx++, &gMtxClear, G_MTX_NOPUSH | G_MTX_LOAD | G_MTX_MODELVIEW);
+
+    Menu_DrawStaticCollision(this, &pGfx, &dGfx, this->pPlay->colCtx.colHeader);
+
+    CLOSE_DISPS(this->pPlay->state.gfxCtx, __FILE__, __LINE__);
 }
