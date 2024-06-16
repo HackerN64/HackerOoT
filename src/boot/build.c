1f201df8415187c9a751ccf1031afef37fe8f12b
Trueffel <106771418+Trueffeloot@users.noreply.github.com>
2024-06-16 02:09:23 +0200

    Makefile update: The "," works now

--- src/boot/build.c
+++ src/boot/build.c
@@ -4,8 +4,8 @@
 #define EXPAND_AND_STRINGIFY(s) STRINGIFY(s)
 
 #if (defined IS_MAP_SELECT_ENABLED && defined PACKAGE_AUTHOR)
-const char* gBuildAuthor = EXPAND_AND_STRINGIFY(PACKAGE_AUTHOR);
-const char* gCommitAuthor = EXPAND_AND_STRINGIFY(PACKAGE_COMMIT_AUTHOR);
+const char* gBuildAuthor = PACKAGE_AUTHOR;
+const char* gCommitAuthor = PACKAGE_COMMIT_AUTHOR;
 #else
 const char* gBuildAuthor = "???";
 const char* gCommitAuthor = "???";
@@ -18,8 +18,8 @@ const char* gBuildDate = "???";
 #endif
 
 #if (defined IS_MAP_SELECT_ENABLED && defined PACKAGE_VERSION)
-const char* gBuildGitVersion = EXPAND_AND_STRINGIFY(PACKAGE_VERSION);
-const char* gCommitGitString = EXPAND_AND_STRINGIFY(PACKAGE_NAME);
+const char* gBuildGitVersion = PACKAGE_VERSION;
+const char* gCommitGitString = PACKAGE_NAME;
 #else
 const char* gBuildGitVersion = "???";
 const char* gCommitGitString = "???";
