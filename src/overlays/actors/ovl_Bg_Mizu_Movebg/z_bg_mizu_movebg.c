640431d91fd772248826316be0ac9e2d56de4271
ariahiro64 <ariahiro64@gmail.com>
2024-06-17 18:29:37 -0400

    made a cardinal sin to make this commit

--- src/overlays/actors/ovl_Bg_Mizu_Movebg/z_bg_mizu_movebg.c
+++ src/overlays/actors/ovl_Bg_Mizu_Movebg/z_bg_mizu_movebg.c
@@ -11,9 +11,9 @@
 #define FLAGS ACTOR_FLAG_4
 
 #define MOVEBG_TYPE(params) (((u16)(params) >> 0xC) & 0xF)
-#define MOVEBG_FLAGS(params) ((u16)(params) & 0x3F)
+#define MOVEBG_FLAGS(params) ((u16)(params)&0x3F)
 #define MOVEBG_PATH_ID(params) (((u16)(params) >> 0x8) & 0xF)
-#define MOVEBG_POINT_ID(params) ((u16)(params) & 0xF)
+#define MOVEBG_POINT_ID(params) ((u16)(params)&0xF)
 #define MOVEBG_SPEED(params) (((u16)(params) >> 0x4) & 0xF)
 
 void BgMizuMovebg_Init(Actor* thisx, PlayState* play);
