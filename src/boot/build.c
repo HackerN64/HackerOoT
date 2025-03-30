2329864870d739dab535306c87ddb63b1212ff25
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-03-30 13:50:38 +0200

    forgot to change gBuild/gCommit in source

--- src/boot/build.c
+++ src/boot/build.c
@@ -3,27 +3,27 @@
 #include "macros.h"
 
 #ifdef PACKAGE_AUTHOR
-const char* gBuildAuthor = EXPAND_AND_STRINGIFY(PACKAGE_AUTHOR);
+const char gBuildAuthor[] = EXPAND_AND_STRINGIFY(PACKAGE_AUTHOR);
 #else
-const char* gBuildAuthor = "???";
+const char gBuildAuthor[] = "???";
 #endif
 
 #ifdef PACKAGE_COMMIT_AUTHOR
-const char* gCommitAuthor = EXPAND_AND_STRINGIFY(PACKAGE_COMMIT_AUTHOR);
+const char gCommitAuthor[] = EXPAND_AND_STRINGIFY(PACKAGE_COMMIT_AUTHOR);
 #else
-const char* gCommitAuthor = "???";
+const char gCommitAuthor[] = "???";
 #endif
 
-const char* gBuildDate = __DATE__ " " __TIME__;
+const char gBuildDate[] = __DATE__ " " __TIME__;
 
 #ifdef PACKAGE_VERSION
-const char* gBuildGitVersion = EXPAND_AND_STRINGIFY(PACKAGE_VERSION);
+const char gBuildGitVersion[] = EXPAND_AND_STRINGIFY(PACKAGE_VERSION);
 #else
-const char* gBuildGitVersion = "???";
+const char gBuildGitVersion[] = "???";
 #endif
 
 #ifdef PACKAGE_NAME
-const char* gCommitGitString = EXPAND_AND_STRINGIFY(PACKAGE_NAME);
+const char gCommitGitString[] = EXPAND_AND_STRINGIFY(PACKAGE_NAME);
 #else
-const char* gCommitGitString = "???";
+const char gCommitGitString[] = "???";
 #endif
