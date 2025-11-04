0eb73e750d49316af68516bb7721f61db4a48bfe
Yanis002 <35189056+Yanis002@users.noreply.github.com>
2025-11-04 04:35:50 +0100

    accidental left-over from merge

--- mod_assets/textures/hud/hud.c
+++
@@ -1,5 +0,0 @@
-#include "hud.h"
-
-u64 gSceneTitleCardGradientTex[] = {
-#include "assets/textures/hud/title_card_gradient.i8.inc.c"
-};
--- mod_assets/textures/hud/hud.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEXTURES_HUD_H
-#define TEXTURES_HUD_H
-
-#include "ultra64/ultratypes.h"
-
-extern u64 gSceneTitleCardGradientTex[];
-
-#endif
